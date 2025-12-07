de59a6d69faabbb2ed2484695ead45b4c740cee4
Nasrin Tavana <en.n.tavana@gmail.com>
2025-12-07 14:58:11 +0330

    feat : added onChange

--- src/components/btnCounter/BtnCounter.tsx
+++ src/components/btnCounter/BtnCounter.tsx
@@ -5,21 +5,32 @@ interface BtnCounter {
   onClick?: () => void;
   className?: string;
   disabled?: boolean;
+  onChange?: (count: number) => void;
 }
 
 function Button({ txtBtn, onClick }: BtnCounter) {
   return <button onClick={onClick}>{txtBtn}</button>;
 }
-
-export default function Counter() {
+export default function Counter({ onChange }: BtnCounter) {
   const [count, setCount] = useState(0);
 
-  const increment = () => setCount((prev) => prev + 1);
-  const decrement = () => setCount((prev) => prev - 1);
+  //#region
+  const increment = () => {
+    const newCount = count + 1;
+    setCount(newCount);
+    onChange?.(newCount);
+  };
 
+  const decrement = () => {
+    const newCount = count - 1;
+    setCount(newCount);
+    onChange?.(newCount);
+  };
+
+  //#region
   const isAdd = count > 0;
   return (
-   <div
+    <div
       className={`
         rounded-md transition-all duration-300 text-center -mt-4
         bg-pink-600 text-white
@@ -30,11 +41,14 @@ export default function Counter() {
       }}
     >
       <div className="flex items-center gap-2 justify-center p-2">
-        {isAdd && <Button txtBtn="-" onClick={decrement} disabled={count <= 0} />}
-        {isAdd && <span className="text-lg font-bold w-8 text-center">{count}</span>}
+        {isAdd && (
+          <Button txtBtn="-" onClick={decrement} disabled={count <= 0} />
+        )}
+        {isAdd && (
+          <span className="text-lg font-bold w-8 text-center">{count}</span>
+        )}
         <Button txtBtn="+" onClick={increment} />
       </div>
     </div>
-
   );
 }
