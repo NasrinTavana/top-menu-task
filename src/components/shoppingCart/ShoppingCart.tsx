aa66dd94853454bd2b0b57ca83fa201e10a1f85a
Nasrin Tavana <en.n.tavana@gmail.com>
2025-12-07 17:13:41 +0330

    feat: edited -> condition for total price

--- src/components/shoppingCart/ShoppingCart.tsx
+++ src/components/shoppingCart/ShoppingCart.tsx
@@ -4,11 +4,11 @@ interface ShoppingCardIn {
   };
 }
 
-//#region 
+//#region
 export default function ShoppingCard({ cart }: ShoppingCardIn) {
-  const total = Object.values(cart).reduce((sum, i) => {
-    return sum + i.product.price * i.count;
-  }, 0);
+  const total = Object.values(cart)
+    .filter((item) => item.product?.price !== undefined)
+    .reduce((sum, item) => sum + item.product.price * item.count, 0);
 
   const totalCount = Object.values(cart).reduce((sum, i) => {
     return sum + i.count;
@@ -16,10 +16,13 @@ export default function ShoppingCard({ cart }: ShoppingCardIn) {
 
   return (
     <div className="w-100 rounded-md bg-pink-600 text-white flex flex-row justify-between p-4 items-center ">
-      <div>
-        <span className="text-xl font-bold">Total:</span>
-        <span className="ml-2 text-2xl font-bold">${total.toFixed(2)}</span>
-      </div>
+      {total > 0 && (
+        <div>
+          <span className="text-xl font-bold">Total:</span>
+          <span className="ml-2 text-2xl font-bold">${total.toFixed(2)}</span>
+        </div>
+      )}
+
       <div>
         <span className="">Complete Purchase</span>
         <span className="rounded-full bg-gray-700 px-3 py-1.5 ml-2">
