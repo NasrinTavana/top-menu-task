200954336e6c82295f76d433e077ab0909a95936
Nasrin Tavana <en.n.tavana@gmail.com>
2025-12-07 14:56:59 +0330

    feat: added index for componnets

---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export { default as ProductCard } from "./ProductCard/ProductCard";
+export { default as ShoppingCard } from "./shoppingCart/ShoppingCart";
+export {default as BtnCounter} from "./btnCounter/BtnCounter";
